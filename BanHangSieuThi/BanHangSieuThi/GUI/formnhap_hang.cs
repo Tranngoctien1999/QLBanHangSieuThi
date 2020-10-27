5e90a98ffa5086231bbd540c97a0952de98a6265
ngoctien <ngoctien18051999@gmail.com>
2020-10-27 21:02:19 +0700

    add form nhaphang

---
+++ BanHangSieuThi/BanHangSieuThi/GUI/formnhap_hang.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using BanHangSieuThi.DataAccess;
+
+namespace BanHangSieuThi.GUI
+{
+    public partial class formnhap_hang : Form
+    {
+        
+        public formnhap_hang()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection;
+        SqlCommand cmd;
+        ConnectString connectStr = new ConnectString();
+        SqlDataAdapter adap = new SqlDataAdapter();
+        DataTable table = new DataTable();
+        void LoadData()
+        {
+            cmd = connection.CreateCommand();
+            cmd.CommandText = "SELECT MaSP,TenSP,TenLH,SoLuong,LoiNhuan,GiaNhap,GiaBan,MoTa,NSX FROM dbo.tblSanPham, dbo.tblLoaihang WHERE tblLoaiHang.MaLH=tblSanPham.MaLH";
+            adap.SelectCommand = cmd;
+            table.Clear();
+            adap.Fill(table);
+            gunaDataGridView1.DataSource = table;
+            
+
+        }
+        private void formnhap_hang_Load(object sender, EventArgs e)
+        {
+            guTbMaSP.Enabled = false;
+            guTbTenSP.Enabled = false;
+            guTbGiaNhapSP.Enabled = false;
+            guTbGiaBanSP.Enabled = false;
+            guTbLoaiSP.Enabled = false;
+            guTbSLSP.Enabled = false;
+            guTbLoiNhuanSP.Enabled = false;
+            guTbMoTa.Enabled = false;
+            guTbNXS.Enabled = false;
+            gunaBtSua.Enabled = false;
+            using (connection = new SqlConnection(connectStr.getConnectionString(fromdangnhap.checkConnectionString)))
+            {
+                connection.Open();
+                LoadData();
+                connection.Close();
+            }
+
+        }
+        int dem = 0;
+        private void gunaButton2_Click(object sender, EventArgs e)
+        {
+            dem = 1;
+            guTbMaSP.Enabled = true;
+            guTbTenSP.Enabled = true;
+            guTbGiaNhapSP.Enabled = true;
+            guTbGiaBanSP.Enabled = true;
+            guTbLoaiSP.Enabled = true;
+            guTbSLSP.Enabled = true;
+            guTbLoiNhuanSP.Enabled = true;
+            guTbMoTa.Enabled = true;
+            guTbNXS.Enabled = true;
+            
+        }
+
+        private void gunaDataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = gunaDataGridView1.CurrentRow.Index;
+            guTbMaSP.Text = gunaDataGridView1.Rows[i].Cells[0].Value.ToString();
+            guTbTenSP.Text = gunaDataGridView1.Rows[i].Cells[1].Value.ToString();
+            guTbLoaiSP.Text = gunaDataGridView1.Rows[i].Cells[2].Value.ToString();
+            guTbSLSP.Text = gunaDataGridView1.Rows[i].Cells[3].Value.ToString();
+            guTbGiaNhapSP.Text = gunaDataGridView1.Rows[i].Cells[4].Value.ToString();
+            guTbGiaBanSP.Text = gunaDataGridView1.Rows[i].Cells[5].Value.ToString();
+            guTbLoiNhuanSP.Text = gunaDataGridView1.Rows[i].Cells[6].Value.ToString();
+            guTbMoTa.Text = gunaDataGridView1.Rows[i].Cells[7].Value.ToString();
+            guTbNXS.Text = gunaDataGridView1.Rows[i].Cells[8].Value.ToString();
+            gunaBtSua.Enabled = true;
+        }
+        string masp = "";
+        private void gunaBtSua_Click(object sender, EventArgs e)
+        {
+            dem = 2;
+            guTbMaSP.Enabled = true;
+            guTbTenSP.Enabled = true;
+            guTbGiaNhapSP.Enabled = true;
+            guTbGiaBanSP.Enabled = true;
+            guTbLoaiSP.Enabled = true;
+            guTbSLSP.Enabled = true;
+            guTbLoiNhuanSP.Enabled = true;
+            guTbMoTa.Enabled = true;
+            guTbNXS.Enabled = true;
+            masp = guTbMaSP.Text;
+        }
+
+        private void gunaBtXoa_Click(object sender, EventArgs e)
+        {
+            using (connection = new SqlConnection(connectStr.getConnectionString(fromdangnhap.checkConnectionString)))
+            {
+                connection.Open();
+                cmd = connection.CreateCommand();
+                cmd.CommandText = "delete dbo.tblSanPham where MaSP='" + guTbMaSP.Text + "'"; 
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Xóa thành công");
+                }
+                catch
+                {
+                    MessageBox.Show("Xóa không thành công");
+                }
+                LoadData();
+                connection.Close();
+            }
+        }
+
+        private void gunaBtLuu_Click(object sender, EventArgs e)
+        {
+            if (dem == 1)
+            {
+                using (connection = new SqlConnection(connectStr.getConnectionString(fromdangnhap.checkConnectionString)))
+                {
+                    connection.Open();
+                    cmd = connection.CreateCommand();
+                    cmd.CommandText = "INSERT INTO dbo.tblSanPham VALUES  ( '" + guTbMaSP.Text + "' , N'" + guTbTenSP.Text + "' , N'LH3' , '" + guTbSLSP.Text + "' , '" + guTbLoiNhuanSP.Text + "' , '" + guTbGiaNhapSP.Text + "' , '" + guTbGiaBanSP.Text + "' , N'" + guTbMoTa.Text + "' , N'" + guTbNXS.Text + "' , N'' , N'LAZADA' )";
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("thêm thành công");
+                        
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Thêm không thành công");
+                        
+                    }
+                    LoadData();
+                    connection.Close();
+                }
+            }
+            if (dem == 2)
+            {
+  
+                using (connection = new SqlConnection(connectStr.getConnectionString(fromdangnhap.checkConnectionString)))
+                {
+                    connection.Open();
+                    cmd = connection.CreateCommand();
+                    cmd.CommandText = "UPDATE dbo.tblSanPham SET  MaSP='" + guTbMaSP.Text + "' ,TenSP=N'" + guTbTenSP.Text + "' , MaLH=N'LH3',  SoLuong='" + guTbSLSP.Text + "' , LoiNhuan='" + guTbLoiNhuanSP.Text + "' , GiaNhap='" + guTbGiaNhapSP.Text + "' , GiaBan='" + guTbGiaBanSP.Text + "' ,MoTa= N'" + guTbMoTa.Text + "' , NSX=N'" + guTbNXS.Text + "' ,HinhAnh=N'' , NhaCC=N'LAZADA' WHERE MaSP='" + masp + "'";
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Sửa thành công");
+       
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Sửa không thành công");
+                        
+                    }
+                    LoadData();
+                    connection.Close();
+                    gunaBtSua.Enabled = false;
+                }
+            }
+            guTbMaSP.Enabled = false;
+            guTbTenSP.Enabled = false;
+            guTbGiaNhapSP.Enabled = false;
+            guTbGiaBanSP.Enabled = false;
+            guTbLoaiSP.Enabled = false;
+            guTbSLSP.Enabled = false;
+            guTbLoiNhuanSP.Enabled = false;
+            guTbMoTa.Enabled = false;
+            guTbNXS.Enabled = false;
+        }
+
+        private void gunaBtHuy_Click(object sender, EventArgs e)
+        {
+            guTbMaSP.Enabled = false;
+            guTbTenSP.Enabled = false;
+            guTbGiaNhapSP.Enabled = false;
+            guTbGiaBanSP.Enabled = false;
+            guTbLoaiSP.Enabled = false;
+            guTbSLSP.Enabled = false;
+            guTbLoiNhuanSP.Enabled = false;
+            guTbMoTa.Enabled = false;
+            guTbNXS.Text = "";
+            guTbMaSP.Text = "";
+            guTbTenSP.Text = "";
+            guTbGiaNhapSP.Text = "";
+            guTbGiaBanSP.Text = "";
+            guTbLoaiSP.Text = "";
+            guTbSLSP.Text = "";
+            guTbLoiNhuanSP.Text = "";
+            guTbMoTa.Text = "";
+            guTbNXS.Text = "";
+        }
+
+        private void gunaDataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = gunaDataGridView1.CurrentRow.Index;
+            guTbMaSP.Text = gunaDataGridView1.Rows[i].Cells[0].Value.ToString();
+            guTbTenSP.Text = gunaDataGridView1.Rows[i].Cells[1].Value.ToString();
+            guTbLoaiSP.Text = gunaDataGridView1.Rows[i].Cells[2].Value.ToString();
+            guTbSLSP.Text = gunaDataGridView1.Rows[i].Cells[3].Value.ToString();
+            guTbGiaNhapSP.Text = gunaDataGridView1.Rows[i].Cells[4].Value.ToString();
+            guTbGiaBanSP.Text = gunaDataGridView1.Rows[i].Cells[5].Value.ToString();
+            guTbLoiNhuanSP.Text = gunaDataGridView1.Rows[i].Cells[6].Value.ToString();
+            guTbMoTa.Text = gunaDataGridView1.Rows[i].Cells[7].Value.ToString();
+            guTbNXS.Text = gunaDataGridView1.Rows[i].Cells[8].Value.ToString();
+            gunaBtSua.Enabled = true;
+        }
+
+        private void guTbLoaiSP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
